918e1e1d38530a2d9ca3d3e8949755b38df554a1
masterccioli <javery@CSELAB5-026.cs.unl.edu>
2015-09-21 17:16:07 -0500

    Note on Products class
    This is in progress -- no need to work on this yet (if you read this
    before I come back to it tonight...). I haven't started on the file-in
    system yet.

--- src/com/airamerica/Products.java
+++ src/com/airamerica/Products.java
@@ -2,6 +2,8 @@ package com.airamerica;
 
 public class Products {
 
-	
+	//John: I'm working on this one, but I'm pausing for yoga :D
+	//I've got the data structures for this mapped out, I
+	//just need to type them up.
 	
 }
