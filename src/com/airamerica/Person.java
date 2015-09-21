28a6581b910742a8ff55757bd556cf34d85aa71d
masterccioli <javery@CSELAB5-026.cs.unl.edu>
2015-09-21 17:00:24 -0500

    Constructors + Getters/Setters
    All necessary fields in all classes except Products. Constructors are
    complete, Getters/Setters are complete. Note in Person.java certain
    fields are optional and excluded from constructor -- will have to use
    Setter to add emails to a Person object.

--- src/com/airamerica/Person.java
+++ src/com/airamerica/Person.java
@@ -9,14 +9,16 @@ public class Person {
 	
 	private String personCode;
 	private String name;
-	private Address address;
-	private String phoneNumber;
+	private Address address; //optional
+	private String phoneNumber; //optional
 	
 	/* Note how email is used (a collection of variable size) */ 
 	private List<String> emails;
 	
 
-	// TODO: Add appropriate constructor(s)
+	/*constructor(s)
+	 * 
+	 */
 	public Person(String personCode, Address address) {
 		this.personCode = personCode;
 		this.address = address;
@@ -28,13 +30,44 @@ public class Person {
 		return this.address;
 	}
 	
-	public void setEmails(List<String> emails) {
-		this.emails = emails;
+	public void setAddress(Address address) {
+		this.address = address;
 	}
-		
+	
+	public String getPersonCode() {
+		return personCode;
+	}
+
+	public void setPersonCode(String personCode) {
+		this.personCode = personCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+
 	// TODO: Add additional methods here
 	public void addEmail(String email) {
 		this.emails.add(email);
+	}
+
+	//TODO: return a formatted list of emails, or return one
+	//		email at a time
+	public List<String> getEmails() {
+		return emails;
 	}
 	
 }
