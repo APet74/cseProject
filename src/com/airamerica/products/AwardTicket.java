eac6216b49a6ef5877990d38133782b1d68511eb
Andy Petrzilka <andypetrzilka@gmail.com>
2015-10-06 14:02:35 -0500

    AwardTicket Fees and Tax

--- src/com/airamerica/products/AwardTicket.java
+++ src/com/airamerica/products/AwardTicket.java
@@ -3,6 +3,7 @@ package com.airamerica.products;
 import java.util.Date;
 
 import com.airamerica.Airport;
+import com.airamerica.utils.Haversine;
 
 public class AwardTicket extends Ticket {
 	private int pointsPerMile;
@@ -22,6 +23,34 @@ public class AwardTicket extends Ticket {
 		this.pointsPerMile = pointsPerMile;
 	}
 	
+	
+	
+	public double getFees(Airport [] airportArray){
+		String seatType = getFlightClass();
+		
+		Airport a1 = getDepAirportCode();
+		Airport a2 = getArrAirportCode();
+		int lat1 = a1.getLattitudeDegrees();
+		int latMin1 = a1.getLattitudeMinutes();
+		int long1 = a1.getLongitudeDegrees();
+		int longMin1 = a1.getLongitudeMinutes();
+		int lat2 = a1.getLattitudeDegrees();
+		int latMin2 = a1.getLattitudeMinutes();
+		int long2 = a1.getLongitudeDegrees();
+		int longMin2 =  a1.getLongitudeMinutes();
+		double fees = 0;
+		
+		double miles = Haversine.getMiles(lat1, latMin1, long1, longMin1, lat2, latMin2, long2, longMin2);
+		if(seatType.equals("EC")){
+			fees = miles * .15;
+		}else if(seatType.equals("BC")){
+			fees = miles * .5;
+		}else{
+			fees = miles * .2;
+		}
+		
+		return fees;
+	}
 	
 	
 }
