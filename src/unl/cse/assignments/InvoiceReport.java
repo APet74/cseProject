17556d975c9d96c41dbe3cab6ab5fc859cb4ff68
Andy Petrzilka <apetrzil@CSELAB5-025.cs.unl.edu>
2015-10-08 15:15:09 -0500

    "Merged" dataConverter with InvoiceReport

--- src/unl/cse/assignments/InvoiceReport.java
+++ src/unl/cse/assignments/InvoiceReport.java
@@ -1,8 +1,18 @@
 package unl.cse.assignments;
 
+import java.util.ArrayList;
+
+import com.airamerica.Airport;
+import com.airamerica.Customer;
+import com.airamerica.Person;
+import com.airamerica.dataConversion.FileReadIn;
+import com.airamerica.dataConversion.ParseData;
+import com.airamerica.invoices.Invoice;
+import com.airamerica.products.Product;
+
 /* Assignment 3,5 and 6 (Project Phase-II,IV and V) */
 
-public class InvoiceReport {
+public class InvoiceReport  {
 	
 	private String generateSummaryReport() {
 		StringBuilder sb = new StringBuilder();
@@ -54,11 +64,77 @@ public class InvoiceReport {
 
 	public static void main(String args[]) {
 
+	    
+		//filename string variable
+		String fileInput = null;
 		
+		/*
+		 * Bring data into memory
+		 */
+		
+		//Airports
+		fileInput = "data/Airports.dat";
+		new FileReadIn();
+		String[] airportsUnparsed = FileReadIn.fileReader(fileInput);
+		
+		//Persons
+		fileInput = "data/Persons.dat";
+		String[] personsUnparsed = FileReadIn.fileReader(fileInput);
+		
+		//Customers
+		fileInput = "data/Customers.dat";
+		String[] customersUnparsed = FileReadIn.fileReader(fileInput);
+		
+		//Products
+		fileInput = "data/Products.dat";
+		String[] productsUnparsed = FileReadIn.fileReader(fileInput);
+		
+		//Invoices
+		fileInput = "data/Invoices.dat";
+		String[] invoicesUnparsed = FileReadIn.fileReader(fileInput);
+		
+		
+		//Person
+		ArrayList<Person> personArray = new ArrayList<Person>();
+		
+		for (int i = 0; i < personsUnparsed.length; i++) {
+			//put it into an array of persons
+			personArray.add(ParseData.parsePerson(personsUnparsed[i]));
+		}
+		
+		//Customer
+		ArrayList<Customer> customerArray = new ArrayList<Customer>();
+		
+		for (int i = 0; i < customersUnparsed.length; i++) {
+			customerArray.add(ParseData.parseCustomer(customersUnparsed[i], personArray));
+		}
+		
+		//Airports
+		ArrayList<Airport> airportArray = new ArrayList<Airport>();
+		for (int i = 0; i < airportsUnparsed.length; i++) {
+			airportArray.add(ParseData.parseAirport(airportsUnparsed[i]));
+		}
+		
+		//Products
+		ArrayList<Product> productArray = new ArrayList<Product>();
+		for (int i = 0; i < productsUnparsed.length; i++) {
+			productArray.add(ParseData.parseProduct(productsUnparsed[i], airportArray, productArray));		
+			//System.out.println(productArray[i].getCode());
+		}
+
+		//Invoices
+		ArrayList<Invoice> invoiceArray = new ArrayList<Invoice>();
+		for (int i = 0; i < invoicesUnparsed.length; i++) {
+			invoiceArray.add(ParseData.parseInvoice(invoicesUnparsed[i], customerArray, personArray, productArray));		
+			//System.out.println(productArray[i].getCode());
+		}
+		
+		
+		//DataConverter.main(); //calls our DataConverter so that it runs and parses all the files.
 		InvoiceReport ir = new InvoiceReport();
 		String summary = ir.generateSummaryReport();
 		String details = ir.generateDetailReport();
-				
+		
 		System.out.println(summary);
 		System.out.println("\n\n");
 		System.out.println(details);
