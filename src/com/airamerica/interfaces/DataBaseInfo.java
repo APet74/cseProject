6e23f01a41c5e40cf77c7b8bf7efb9df62a6cacb
Andy Petrzilka <apetrzil@CSELAB5-025.cs.unl.edu>
2015-11-11 10:58:35 -0600

    test

---
+++ src/com/airamerica/interfaces/DataBaseInfo.java
@@ -0,0 +1,52 @@
+package com.airamerica.interfaces;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DataBaseInfo {
+
+
+
+		
+
+
+
+			public static final String url = "jdbc:mysql://cse.unl.edu/javery";
+			public static final String username = "javery";
+			public static final String password = "jellyfish";
+			
+			static public Connection getConnection()
+			{
+				try {
+					Class.forName("com.mysql.jdbc.Driver").newInstance();
+				} catch (InstantiationException e) {
+					System.out.println("InstantiationException: ");
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				} catch (IllegalAccessException e) {
+					System.out.println("IllegalAccessException: ");
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				} catch (ClassNotFoundException e) {
+					System.out.println("ClassNotFoundException: ");
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+				
+				Connection conn = null;
+
+				try {
+					conn = DriverManager.getConnection(DataBaseInfo.url, DataBaseInfo.username, DataBaseInfo.password);
+				} catch (SQLException e) {
+					System.out.println("SQLException: ");
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+				
+				return conn;
+	}
+}
+
+
