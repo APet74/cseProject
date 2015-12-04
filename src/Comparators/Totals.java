1d5661b708b5eb581fcb324af36321ced998a135
Andy Petrzilka <apetrzil@CSELAB5-025.cs.unl.edu>
2015-12-04 10:08:52 -0600

    1111

--- src/Comparators/Totals.java
+++ src/Comparators/Totals.java
@@ -2,12 +2,21 @@ package Comparators;
 
 import java.util.Comparator;
 
-public class Totals implements Comparator{
+import com.airamerica.invoices.Invoice;
+
+public class Totals implements Comparator<Invoice>{
 
 	@Override
-	public int compare(Object arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compare(Invoice inv1, Invoice inv2) {
+		if(inv2.getTotal(inv2) > inv1.getTotal(inv1)){
+			return -1;
+		}else if(inv2.getTotal(inv2) < inv1.getTotal(inv1)){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+
 	}
 
 }
